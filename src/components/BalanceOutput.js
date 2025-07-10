2ece3f1cb4ea0ec6a048caa92874f7b5bc446345
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2025-07-10 10:54:18 -0600

    Initial commit

---
+++ src/components/BalanceOutput.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as utils from '../utils';
+
+class BalanceOutput extends Component {
+  render() {
+    if (!this.props.userInput.format) {
+      return null;
+    }
+
+    return (
+      <div className='output'>
+        <p>
+          Total Debit: {this.props.totalDebit} Total Credit: {this.props.totalCredit}
+          <br />
+          Balance from account {this.props.userInput.startAccount || '*'}
+          {' '}
+          to {this.props.userInput.endAccount || '*'}
+          {' '}
+          from period {utils.dateToString(this.props.userInput.startPeriod)}
+          {' '}
+          to {utils.dateToString(this.props.userInput.endPeriod)}
+        </p>
+          {this.props.balance.length === 0 ? (
+            <p>No results found.</p>
+          ) : (
+            <div>
+              {this.props.userInput.format === 'CSV' ? (
+                <pre>{utils.toCSV(this.props.balance)}</pre>
+              ) : null}
+              {this.props.userInput.format === 'HTML' ? (
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>ACCOUNT</th>
+                      <th>DESCRIPTION</th>
+                      <th>DEBIT</th>
+                      <th>CREDIT</th>
+                      <th>BALANCE</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.props.balance.map((entry, i) => (
+                      <tr key={i}>
+                        <th scope="row">{entry.ACCOUNT}</th>
+                        <td>{entry.DESCRIPTION}</td>
+                        <td>{entry.DEBIT}</td>
+                        <td>{entry.CREDIT}</td>
+                        <td>{entry.BALANCE}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ) : null}
+              </div>
+          )}
+          </div>
+    )
+        
+  }
+}
+
+BalanceOutput.propTypes = {
+  balance: PropTypes.arrayOf(
+    PropTypes.shape({
+      ACCOUNT: PropTypes.number.isRequired,
+      DESCRIPTION: PropTypes.string.isRequired,
+      DEBIT: PropTypes.number.isRequired,
+      CREDIT: PropTypes.number.isRequired,
+      BALANCE: PropTypes.number.isRequired
+    })
+  ).isRequired,
+  totalCredit: PropTypes.number.isRequired,
+  totalDebit: PropTypes.number.isRequired,
+  userInput: PropTypes.shape({
+    startAccount: PropTypes.number,
+    endAccount: PropTypes.number,
+    startPeriod: PropTypes.date,
+    endPeriod: PropTypes.date,
+    format: PropTypes.string
+  }).isRequired
+};
+
+export default connect(state => {
+  let balance = [];
+
+  /* YOUR CODE GOES HERE */
+  const { journalEntries = [], accounts = [], userInput } = state;
+
+  const {
+    startAccount,
+    endAccount,
+    startPeriod,
+    endPeriod
+  } = userInput;
+  
+  const filteredJournals = journalEntries.filter(item=>{
+    return item.ACCOUNT >= startAccount && item.ACCOUNT <= endAccount && item.PERIOD >= new Date(startPeriod) && item.PERIOD <= new Date(endPeriod)
+  })
+
+  balance = filteredJournals.map((journal) => {
+    const accountInfo = accounts.find(acc => acc.ACCOUNT === journal.ACCOUNT) || {};
+  
+    return {
+      ACCOUNT: journal.ACCOUNT,
+      DESCRIPTION: accountInfo.LABEL || '',
+      DEBIT: journal.DEBIT,
+      CREDIT: journal.CREDIT,
+      BALANCE: journal.DEBIT - journal.CREDIT
+    };
+  });
+
+  const totalCredit = balance.reduce((acc, entry) => acc + entry.CREDIT, 0);
+  const totalDebit = balance.reduce((acc, entry) => acc + entry.DEBIT, 0);
+
+  return {
+    balance,
+    totalCredit,
+    totalDebit,
+    userInput: state.userInput
+  };
+})(BalanceOutput);
---
+++ src/components/InputForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { accounts, journal } from '../data';
+import * as actions from '../actions';
+import * as utils from '../utils';
+
+class InputForm extends Component {
+  state = {
+    accounts,
+    journal,
+    userInput: '1000 5000 MAR-16 JUL-16 HTML',
+    error: ""
+  };
+
+  componentDidMount() {
+    this.handleSubmit();
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  handleSubmit = e => {
+    e && e.preventDefault();
+
+
+    if (this.state.userInput.trim() === "") {
+      alert("Please fill in the User input field.");
+      this.setState({ error: "User input is required." });
+      return;
+    }
+    this.setState({ error: "" });
+
+    this.props.dispatch(actions.setJournalEntries(utils.parseCSV(this.state.journal)));
+    this.props.dispatch(actions.setAccounts(utils.parseCSV(this.state.accounts)));
+    this.props.dispatch(actions.setUserInput(utils.parseUserInput(this.state.userInput)));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor="journal">Journal</label>
+          <textarea className="form-control" id="journal" rows="3" value={this.state.journal} onChange={this.handleChange}></textarea>
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor="accounts">Accounts</label>
+          <textarea className="form-control" id="accounts" rows="3" value={this.state.accounts} onChange={this.handleChange}></textarea>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="userInput">User input</label>
+          <input type="text" className="form-control" id="userInput" value={this.state.userInput} onChange={this.handleChange} />
+          {this.state.error && <small className="text-danger">{this.state.error}</small>}
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default connect()(InputForm);
